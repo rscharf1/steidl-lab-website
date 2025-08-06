708d3510ec43efe76bb5dcbb53bf7846811304f7
rscharf1 <rscharf33@gmail.com>
2025-08-06 14:32:56 -0400

    eleventy.js and deploy updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 module.exports = function (eleventyConfig) {
-  // Keep copying assets to _site
+  // Copy static assets
   eleventyConfig.addPassthroughCopy("./assets");
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy(".nojekyll");
 
   return {
     dir: {
-      input: ".",        // or "src" if you’re using a source folder
+      input: ".",   // or "src" if you're using a source folder
       output: "_site"
     },
-    pathPrefix: "/steidl-lab-website/"
+    pathPrefix: ""  // Empty for custom domain (not /repo-name/)
   };
 };
+
